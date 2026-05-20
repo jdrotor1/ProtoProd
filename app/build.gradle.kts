0b1cd6f039b323900a112efa0df7572196246157
jdrotor1 <joel.drotor@gmail.com>
2026-05-19 23:58:30 -0700

    Add material3-icons dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 
     val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
     implementation(composeBom)
+    implementation("androidx.compose.material3:material3-icons")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
